15793a45989fcf31d90b93d2d742e884f1d29484
icy1xtee <easton2904@mail.ru>
2024-02-01 20:30:24 +0300

    radio button fixed, validation of empty added

--- routes/api/themesApi.route.js
+++ routes/api/themesApi.route.js
@@ -7,12 +7,15 @@ const { Answer } = require('../../db/models');
 router.post('/', async (req, res) => {
   try {
     const { id } = req.body;
-    const answer = await Answer.findOne({ where: { id } });
-
-    if (answer.isRight) {
-      res.json({ message: 'Правильно' });
+    if (id === '') {
+      res.json({ message: 'Необходимо выбрать' });
     } else {
-      res.json({ message: 'Не правильно' });
+      const answer = await Answer.findOne({ where: { id } });
+      if (answer.isRight) {
+        res.json({ message: 'Правильно' });
+      } else {
+        res.json({ message: 'Не правильно' });
+      }
     }
   } catch ({ message }) {
     res.json({ message });
